360b68b7e909fe689aa2690fc44ee64747caaad9
ukulsum <ukulsum1280@gmail.com>
2025-04-01 14:21:12 +0600

    bindparamer

--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,7 +1,35 @@
+using MinimalApiDemo;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.MapGet("/", () =>
 {
     return "Hello World";
+});
+
+app.MapPost("/", () =>
+{
+    return Results.Ok("Call Post action method.");
+});
+
+app.MapPut("/", () =>
+{
+    return Results.Ok("Call put action method.");
+});
+
+app.MapDelete("/", () =>
+{
+    return Results.Ok("Call delete action method");
+});
+
+// Bind Parameter
+app.MapPost("Parameter", (string name) =>
+{
+    return Results.Ok("Hello" + name);
+});
+
+app.MapPost("post", (Post post) =>
+{
+    return Results.Ok(post);
 });
 app.Run();
